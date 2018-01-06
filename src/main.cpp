38e8e2112e431d38b0412faac53a6e159d896a48
MCMrARM <mcmrarm@gmail.com>
2018-01-06 15:34:29 +0100

    Use 3.2 GL context

--- src/main.cpp
+++ src/main.cpp
@@ -669,6 +669,11 @@ int main(int argc, char *argv[]) {
     Social::MultiplayerXBL::MultiplayerXBL_MultiplayerXBL = (void (*)(Social::MultiplayerXBL*)) hybris_dlsym(handle, "_ZN6Social14MultiplayerXBLC2Ev");
 
     std::cout << "init window\n";
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     window = glfwCreateWindow(windowWidth, windowHeight, "Minecraft", NULL, NULL);
     if (!window)
     {
